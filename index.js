2aa8034f2acdca7ec83a3def4bd4f0d3e89da27f
SpecsNStats <specsnstats@gmail.com>
2021-10-12 19:58:26 -0600

    added the code

--- index.js
+++ index.js
@@ -1,13 +1,126 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require('fs')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Project Title:',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Project Description:',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Installation Instructions:',
+            name: 'installation'
+        },
+        {
+            type: 'input',
+            message: 'Usage Information:',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'Contribution Guidelines:',
+            name: 'contribution'
+        },
+        {
+            type: 'input',
+            message: 'Test Instructions:',
+            name: 'test'
+        },
+        {
+            type: 'list',
+            message: 'License Type:',
+            name:'license',
+            choices: [
+                {name: 'Apache'},
+                {name: 'Boost'},
+                {name: 'Eclipse'},
+                {name: 'GNU'},
+                {name: 'MIT'},
+                {name: 'WTFPL'},
+            ]
+        },
+        {
+            type: 'input',
+            message: 'GitHub Username',
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: 'Email Address:',
+            name: 'email'
+        },
+    ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    .then(({title,description,installation,usage,contribution,test,license,github,email}) => {
+        switch(license) {
+            case "Apache":
+                var licenseType =  `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+                break;
 
-// TODO: Create a function to initialize app
-function init() {}
+            case "Boost":
+                var licenseType = `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+                break;
 
-// Function call to initialize app
-init();
+            case 'Eclipse':
+                var licenseType = `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+                break;
+            
+            case "GNU":
+                var licenseType = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+                break;
+
+            case "MIT":
+                var licenseType = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+                break;
+            
+            case 'WTFPL':
+                var licenseType = `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`
+
+        }
+        console.log(licenseType);
+        const resultingReadme = 
+        `# ${title}
+
+## Table of Contents
+
+- [Project Description](./newREADME#project-description)
+- [Installation Guide](./newREADME#installation-guide)
+- [Usage Information](./newREADME#usage-information)
+- [Contribution Guidelines](./newREADME#contribution-guidelines)
+- [Test Instructions](./newREADME#test-instructions)
+- [Questions](./newREADME#questions)
+
+## Project Description:
+${description}
+
+## Installation Guide:
+${installation}
+
+## Usage Information:
+${usage}
+
+## Contribution Guidelines:
+${contribution}
+
+## Test Instructions:
+${test}
+
+## License Type:
+${licenseType}
+
+## Questions:
+${github}
+${email}`
+
+        fs.writeFile("newREADME.md", resultingReadme, (err) =>
+        err? console.log(error): console.log("README Generated!"));
+    });
+
---
+++ newREADME.md
@@ -0,0 +1,32 @@
+# README Generator
+ 
+## Table of Contents
+
+- [Project Description](./newREADME#project-description)
+- [Installation Guide](./newREADME#installation-guide)
+- [Usage Information](./newREADME#usage-information)
+- [Contribution Guidelines](./newREADME#contribution-guidelines)
+- [Test Instructions](./newREADME#test-instructions)
+- [Questions](./newREADME#questions)
+
+## Project Description:
+This project is designed to create a README from a list of prompts given to a user.
+
+## Installation Guide:
+after cloning the repository to your own local files, simply install Inquirer like so: ```npm i inquirer```
+
+## Usage Information:
+after you have successfully installed the program, simply navigate to the folder that the program is located in through your command prompt. Once there, you can run the program by typing ```node index.js```. The program will give you a series of prompts for you to answer as it applies to the project you would like to create a README for, and once all the prompts are completed, the program will generate the new README with the name "newREADME.md". 
+
+## Contribution Guidelines:
+If you wanna contribute, you could probably add more prompts or information, or even play around with the layout of the resultingReadme variable to make it look prettier.
+
+## Test Instructions:
+No real test instructions to put here...
+
+## License Type:
+[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
+
+## Questions:
+https://www.github.com/specsnstats
+specsnstats@gmail.com
